30d51e16b87c168369ae96f5cee236a1f37f0f1d
rende.zhao <351157970@qq.com>
2018-10-17 21:22:32 +0800

    v1.1.0
    1. 添加登录功能
    2. 添加多语言
    3. 完善菜单功能
    4. 添加复用tab

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae91fb3bb6cfe7cba4ac6ef3e6404f97.js"
+  "/precache-manifest.5e25e959214c15aa6f4c73277dddcdce.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-alain"});
